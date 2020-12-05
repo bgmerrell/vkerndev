b6b9c25a7f5528c3eb94084dab63b027331e064a
Brian G. Merrell <brian.merrell@walmartlabs.com>
2020-12-05 04:44:36 -0700

    working bpf/xdp dev env scripts

--- run_vm.sh
+++ run_vm.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+# optional shared folder for arbitrary sharing
+shared_folder=/home/bgmerrell/dev/shared
+
+# build kernel image and headers on your host and use them on your guest
+kernel_image=/home/bgmerrell/code/linux/arch/x86/boot/bzImage
+kernel_headers=/home/bgmerrell/code/linux/usr/include
+
 qemu-system-x86_64 \
 	-boot order=a -drive file=arch_disk.vm,format=raw,if=virtio \
-	-nographic -kernel \
- 	/home/bgmerrell/code/linux/arch/x86/boot/bzImage \
+	-nographic -kernel $kernel_image \
 	-append "root=/dev/vda rw console=ttyS0 loglevel=7 raid=noautodetect" \
-	-fsdev local,id=fs1,path=/home/bgmerrell/dev/shared,security_model=none \
+	-fsdev local,id=fs1,path=$shared_folder,security_model=none \
 	-device virtio-9p-pci,fsdev=fs1,mount_tag=shared_folder \
+	-fsdev local,id=fs2,path=$kernel_headers,security_model=none \
+	-device virtio-9p-pci,fsdev=fs2,mount_tag=linux_headers \
 	--enable-kvm -m 4G -smp 2 -cpu host \
-	-net nic,model=virtio -net user,hostfwd=tcp::1313-:22
+	-net nic,model=e1000 -net user,hostfwd=tcp::1313-:22
--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # enable and start service
 services="sshd dhcpcd"
 systemctl enable $services
-systemctl start $services
+#systemctl start $services
 
 # place ssh public key
 mkdir .ssh
@@ -16,6 +16,11 @@ echo "[Service]" > "/etc/systemd/system/serial-getty@.service.d/override.conf"
 echo "ExecStart=" >> "/etc/systemd/system/serial-getty@.service.d/override.conf"
 echo 'ExecStart=-/usr/bin/agetty --autologin root --noclear %I $TERM' >> "/etc/systemd/system/serial-getty@.service.d/override.conf"
 
+# setup bash
+echo "alias ls='ls --color=auto'" >> /root/.bashrc
+echo "export PS1='🦄 \[\e[32m\]\u@\h: \[\e[33m\]\w\[\e[0m\]\n\\$ '" >> /root/.bashrc
+echo "[[ -f ~/.bashrc ]] && . ~/.bashrc" >> /root/.bash_profile
+
 # enable X11 forwarding
 echo "X11Forwarding yes" >> /etc/ssh/sshd_config
 
@@ -25,7 +30,11 @@ mkdir /home/user/.ssh
 cp /root/.ssh/authorized_keys /home/user/.ssh/authorized_keys
 echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
+# Remove installed linux headers package. We mount the host headers below
+pacman -Rnsdd --noconfirm linux-api-headers
+
 # fill the fstab
 echo "shared_folder /home/user/host 9p trans=virtio 0 0" >> /etc/fstab
+echo "linux_headers /usr/local/include 9p trans=virtio 0 0" >> /etc/fstab
 
-reboot
+#reboot
