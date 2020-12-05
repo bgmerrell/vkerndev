969a0bf1e69830b781ac14bf2a0c734bc4590fec
Brian G. Merrell <brian.merrell@walmartlabs.com>
2020-12-05 04:44:36 -0700

    working bpf/xdp dev env scripts

--- deploy_vm.sh
+++ deploy_vm.sh
@@ -2,7 +2,7 @@
 disk_space=${1:-'8G'}
 disk_name=${2:-'arch_disk.vm'}
 
-extra_packs='vim git strace gdb dhcpcd openssh'
+extra_packs='vim git strace gdb dhcpcd openssh clang llvm pahole'
 xforwarding_packs='xorg-xauth xorg-xclock xorg-fonts-type1 haveged'
 
 all_packs="$extra_packs $xforwarding_packs"
@@ -14,7 +14,7 @@ mkfs.ext4 $disk_name
 # mount disk
 mkdir mnt
 sudo mount $disk_name mnt
-sleep 3
+sleep 1
 
 # install basic system
 sudo pacstrap -c mnt base base-devel $all_packs
@@ -23,7 +23,13 @@ sudo pacstrap -c mnt base base-devel $all_packs
 sudo cp ~/.ssh/id_rsa.pub mnt/root/
 
 # copy bootstrap script
-sudo cp start.sh mnt/root/
+sudo cp setup.sh mnt/root/
 
 # umount disk
+sudo umount mnt
+
+# Run setup.sh script
+sudo mount $disk_name mnt
+sleep 1
+sudo arch-chroot mnt /bin/bash -c "cd /root && ./setup.sh && rm setup.sh"
 sudo umount mnt
