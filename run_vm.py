31de0c37476c085dc7ee8d31b81f81de32be87e0
Brian G. Merrell <bgmerrell@gmail.com>
2022-03-03 11:56:03 -0700

    Add options for VM memory and num cpus

--- run_vm.py
+++ run_vm.py
@@ -10,25 +10,34 @@ import sys
 def parse_args():
     parser = argparse.ArgumentParser(description='Run qemu VM')
     parser.add_argument(
-        '--kernel', dest='kernel_path',
+        '-k', '--kernel', dest='kernel_path',
         help='path to the kernel to use on the VM',
         default=os.path.join(
             os.path.expanduser('~'), 'code/linux/arch/x86/boot/bzImage'))
     parser.add_argument(
-        '--shared-folder', dest='shared_folder',
+        '-s', '--shared-folder', dest='shared_folder',
         help='path to a directory that will be shared with the VM',
         default=os.path.join(os.path.expanduser('~'), 'dev/shared'))
     parser.add_argument(
-        '--kernel-headers', dest='kernel_headers_path',
-        help='path to the kernel headers to use for the VM ',
+        '-j', '--kernel-headers', dest='kernel_headers_path',
+        help='path to the kernel headers to use for the VM',
         default=os.path.join(
             os.path.expanduser('~'), 'code/linux/usr/include'))
+    parser.add_argument(
+        '-m', '--memory', dest='vm_memory',
+        help='Amount of memory to use on the VM using a "M" or "G" suffix to '
+             'signify megabytes or gigabytes respectively',
+        default='2G')
+    parser.add_argument(
+        '-c', '--cpus', dest='vm_num_cpus',
+        help='Number of CPUs for the VM', type=int, default='2')
     parser.add_argument(
         '--vm', dest='vm_path',
         help='path to the vm image', default='arch_disk.vm')
     parser.add_argument(
-        '--debug', action=argparse.BooleanOptionalAction,
-        help='print debug messages')
+        '--dry-run', action='store_true', help="Don't actually exec qemu")
+    parser.add_argument(
+        '--debug', action='store_true', help='print debug messages')
     return parser.parse_args()
 
 
@@ -37,24 +46,24 @@ def main():
     args = parse_args()
     if args.debug:
         logging.getLogger().setLevel(logging.DEBUG)
-    qemu_cmd_str = \
-        'qemu-system-x86_64 ' \
-        f'-boot order=a -drive file={args.vm_path},format=raw,if=virtio ' \
-        f'-nographic -kernel {args.kernel_path} ' \
-        '-append "root=/dev/vda rw console=ttyS0 loglevel=7 '\
-            'raid=noautodetect" ' \
-        f'-fsdev local,id=fs1,path={args.shared_folder},security_model=none ' \
-        '-device virtio-9p-pci,fsdev=fs1,mount_tag=shared_folder ' \
-        f'-fsdev local,id=fs2,path={args.kernel_headers_path},' \
-            'security_model=none ' \
-        '-device virtio-9p-pci,fsdev=fs2,mount_tag=linux_headers ' \
-        '--enable-kvm -m 4G -smp 2 -cpu host ' \
-        '-net nic,model=e1000 -net user,hostfwd=tcp::1313-:22'
+    qemu_cmd_str = ('qemu-system-x86_64 '
+        f'-boot order=a -drive file={args.vm_path},format=raw,if=virtio '
+        f'-nographic -kernel {args.kernel_path} '
+        '-append "root=/dev/vda rw console=ttyS0 loglevel=7 '
+            'raid=noautodetect" '
+        f'-fsdev local,id=fs1,path={args.shared_folder},security_model=none '
+        '-device virtio-9p-pci,fsdev=fs1,mount_tag=shared_folder '
+        f'-fsdev local,id=fs2,path={args.kernel_headers_path},'
+            'security_model=none '
+        '-device virtio-9p-pci,fsdev=fs2,mount_tag=linux_headers '
+        f'--enable-kvm -m {args.vm_memory} -smp {args.vm_num_cpus} -cpu host '
+        '-net nic,model=e1000 -net user,hostfwd=tcp::1313-:22')
     qemu_cmd = shlex.split(qemu_cmd_str, posix=True)
     logging.debug(f'Running: {qemu_cmd}')
     sys.stdout.flush()
     sys.stderr.flush()
-    os.execvp('qemu-system-x86_64', qemu_cmd)
+    if not args.dry_run:
+        os.execvp('qemu-system-x86_64', qemu_cmd)
 
 
 if __name__ == '__main__':
